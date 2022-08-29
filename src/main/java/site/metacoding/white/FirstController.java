d45bb9f7ed9949da5f8068b9950b43850b657f0e
sdics <jhnadas@naver.com>
2022-08-29 15:04:27 +0900

    스프링부트 컨트롤러 데이터 받기 기본

--- src/main/java/site/metacoding/white/FirstController.java
+++ src/main/java/site/metacoding/white/FirstController.java
@@ -17,7 +17,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class FirstController {
 	@GetMapping("/first")
 	public String getData() {
-		
 		return "<h1>data</h1>";
 	}
 	
---
+++ src/main/java/site/metacoding/white/SecondController.java
@@ -0,0 +1,41 @@
+package site.metacoding.white;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+// 데이터 받기
+// GET -> http body가 없음. -> QueryString,PathVariable -> Form(Get) -> 브라우저 ( 주소뒤 ? 붙이기 )
+// POST , PUT -> http body 담아준다. -> Form(POST)
+// DELETE  -> http body가 없음. -> QuerySttring, PathVariable(PK)
+@RestController
+public class SecondController {
+	@GetMapping("/second/{id}") // PK(id)가 1인것
+	public String getData(@PathVariable Integer id) {
+		return "id : " + id;
+	}
+	
+	// QueryString = x-www-form-urlencoded 타입이다.
+	@GetMapping("/second")
+	public String getData2(String title, String content) {
+		return "title : " + title + " , content : "+ content;
+	}
+	// BR - > title=제목&content=내용 , content-type
+	@PostMapping("second")
+	public String postData(String title, String content) { // 스프링 파싱 기본전략 : x-www-form-머시기
+		return "title : " + title + " , content : "+ content;
+	}
+	
+	@PutMapping("/second")
+	public String putData(String title, String content) { 
+		return "title : " + title + ", content : "+ content;
+	}
+	
+	@DeleteMapping("/second{id}")
+	public String deleteData(@PathVariable Integer id) {
+		return id + "delete ok";
+	}
+}
